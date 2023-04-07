a09883adc357bef1b5da8d6a951d5bfc7a7f6f53
Foursharks <69390429+Foursharks@users.noreply.github.com>
2023-04-07 17:00:36 -0400

    Initial Commit and Solution

---
+++ maxOfTwo.js
@@ -0,0 +1,16 @@
+let x = 2
+let y = 3
+
+// Write your code below this line.
+
+if (x > y){
+    console.log(x); 
+}
+    else if (x < y)
+    {
+        console.log(y);
+    }
+        else {
+            console.log('x and y are the same');
+        }
+    
